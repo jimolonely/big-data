9fe2d790cac447c3c0a921bedafbab0e317ba3ee
jimo <jimolonely@yahoo.com>
2019-02-10 19:52:10 +0800

    add java scala

--- scala-practice/src/main/java/com/jimo/HelloWorld.scala
+++ scala-practice/src/main/java/com/jimo/HelloWorld.scala
@@ -1,7 +1,14 @@
 package com.jimo
 
+import java.text.DateFormat
+import java.util.{Date, Locale}
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("hello world")
+
+    val now = new Date
+    val df = DateFormat.getDateInstance(DateFormat.LONG, Locale.CHINA)
+    println(df format now)
   }
 }
